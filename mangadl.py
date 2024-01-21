dcf3977b7728d00fa7a4d593820fcca82bbbcebb
JKCrafted <78131640+JKCrafted@users.noreply.github.com>
2024-01-21 14:15:19 +1100

    Updated scripts in github database

---
+++ mangadl.py
@@ -0,0 +1,29 @@
+import urllib
+import requests
+bookno = 0
+pageno = 10001
+failno = 0
+langver = input("EN or CN: ")
+if langver.upper() == "EN":
+    mangano = input("Manga ID here (https://manga.honkaiimpact3.com/book/id): ")
+else:
+    mangano = input("Manga ID here (https://comic.bh3.com/book/id): ")
+while True:
+    if langver.upper() == "EN":
+        url = "https://d2tpbmzklky1cl.cloudfront.net/manga/static/comic/book/"+str(mangano)+"/"+str(bookno)+"/"+(str(pageno))[1:5]+".jpg"
+    else:
+        url = "https://act-webstatic.mihoyo.com/new_static_v2/comic/book/"+str(mangano)+"/"+str(bookno)+"/"+(str(pageno))[1:5]+".jpg"
+    img_url = url
+    print(img_url)
+    try:
+        urllib.request.urlretrieve(img_url, str(mangano)+str(bookno)+str(pageno)[1:5]+".jpg")
+        pageno = int(pageno)+1
+        failno = 0
+    except:
+        bookno = bookno+1
+        pageno = 10001
+        failno = failno+1
+    #print(failno)
+    if failno == 5:
+        break
+print("Full Download Complete")
--- replace.js
+++ replace.js
@@ -1,29 +1,49 @@
-function LangReplace() {
-    fetch("https://raw.githubusercontent.com/Hoyostans/MangaTranslations/main/AWtest_CN.txt")
+<button type="button" onclick="LangReplace()">Change Language</button> <div class="selectedlanguagetext"><p>0</p></div>
+<script>function LangReplace() {
+    var doc_lang_id = document.getElementsByClassName("selectedlanguagetext")[0];
+    var doc_lang_id_node = doc_lang_id.getElementsByTagName('p')[0];
+    var sel_lang_buffer = doc_lang_id_node.outerHTML.replace('<p>','');
+    var sel_lang_buffer = sel_lang_buffer.replace('</p>','');
+    var sel_lang = parseInt(sel_lang_buffer);
+    try {
+    if (sel_lang < languages.length-1) {
+        sel_lang++
+    }
+    else {
+        var sel_lang = 0;
+    }
+    }
+    catch(err) {
+        var sel_lang = 0;
+    }
+    var sel_lang_id = languages[sel_lang];
+    var linkid = 'https://raw.githubusercontent.com/Hoyostans/MangaTranslations/main/'+manid+'_'+sel_lang_id+'.txt';
+    try {
+    fetch(linkid)
         .then(function (res) {
             return res.text();
         })
         .then(function (txtdata) {
-            console.log(txtdata)
             var textdata = txtdata.split("\n");
-            console.log(textdata)
             var text_items = document.getElementsByClassName("e-hotspot__direction-mask e-hotspot--tooltip-position");
-    console.log(text_items)
     
     for (var q=0; q<text_items.length; q++)
     {  
       var text_node = text_items[q];
-      console.log(text_node)
       var text_node_node = text_node.getElementsByTagName('p')[0];
-      console.log(text_node_node)
       text_node_node.innerHTML = '<p>'+ textdata[q] + '</p>'
-      console.log(text_node_node)
     }
+      doc_lang_id_node.innerHTML = sel_lang
+
+
         });
     
-    
-    
-    console.log(text_data)
-    
-    
     }
+    catch(err) {
+        sel_lang++
+        call.LangReplace();
+    }
+
+
+
+    }</script>
